8d2ded7486976d7e8988ea0a509bfb219242df74
Colin Breeding <colinmbreeding@gmail.com>
2023-07-05 18:16:11 -0400

    update dark background color

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${font.className} -bg-white dark:-bg-smoothBlack duration-100 ease-in-out`}
+        className={`${font.className} -bg-white dark:bg-[#0a0a0a] duration-100 ease-in-out`}
       >
         <ThemeProvider>
           <AuthModalProvider>
