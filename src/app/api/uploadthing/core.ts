c800ec3e336c28b89ad340f66aa5c504b16728db
Colin Breeding <colinmbreeding@gmail.com>
2023-07-11 21:28:42 -0400

    fix image upload error

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,12 +2,12 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
 
-const auth = () => ({ id: "fakeId" });
+const auth = (req: Request) => ({ id: "fakeId" });
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
-      const user = await auth(req);
+      const user = auth(req);
 
       if (!user) throw new Error("Unauthorized");
 
